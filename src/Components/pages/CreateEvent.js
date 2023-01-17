42dc84b3f3886d6087c97f9bdba284ca08645ab7
Viktoria <viktoria.grigoriev@hm.edu>
2023-01-18 00:52:51 +0100

    CreateEvent

--- src/Components/pages/CreateEvent.js
+++ src/Components/pages/CreateEvent.js
@@ -3,32 +3,70 @@ import "@fontsource/outfit";
 import "./CreateEvent.css"
 
 
+
+import { useState } from 'react';
+
 function CreateEvent() {
+  const [title, setTitle] = useState("");
+  const [about, setAbout] = useState("");
+  const [when, setWhen] = useState("");
+  const [where, setWhere] = useState("");
+  const [people, setPeople] = useState("");
+
+  const handleCreate = async () => {
+    const url = "http://localhost:3000/events"
+    const token = "";
+    const requestOptions = {
+      method:"POST",
+      headers: {
+        "Content-Type":"application/json",
+        "Authorization": `Bearer ${token}`
+    },
+
+      body:JSON.stringify({
+        Title: title,
+        About: about,
+        Date: when,
+        Location: where,
+        VisitorNumber: people
+      })
+    }
+    try {
+        const response = await fetch(url, requestOptions);
+        const json = await response.json();
+        if(!response.ok) {
+            throw new Error(json.message);
+        }
+        console.log("Event Created Successfully");
+    } catch (error) {
+        console.log(error);
+    }
+  }
   return (
     <div class='form-wrapper' className='fit' style={{ fontFamily: "outfit" }}>
-      <p>TITLE</p >
+      <p>TITLE</p>
       <form class="formstyle">
-        <input type="text" id="input-field" name="input-field" class="inputline"/>
+        <input type="text" value={title} onChange={e => setTitle(e.target.value)} class="inputline"/>
       </form>
 
       <p>ABOUT</p>
       <form class="formstyle">
-        <input type="text" id="input-field" name="input-field" class="inputline"/>
+        <input type="text" value={about} onChange={e => setAbout(e.target.value)}class="inputline"/>
       </form>
 
       <p>WHEN?</p>
       <form class="formstyle">
-        <input type="text" id="input-field" name="input-field" class="inputline"/>
+        <input type="text" value={when} onChange={e => setWhen(e.target.value)} class="inputline"/>
       </form>
 
       <p>WHERE?</p>
       <form class="formstyle">
-        <input type="text" id="input-field" name="input-field" class="inputline"/>
+        <input type="text" value={where} onChange={e => setWhere(e.target.value)} class="inputline"/>
       </form>
 
       <p>FOR HOW MANY PEOPLE?</p>
       <form class="formstyle">
-        <input type="text" id="input-field" name="input-field" class="inputline"/>
+        <input type="text" value={people} onChange={e => setPeople(e.target.value)} class="inputline"/>
       </form>
 
       <p>PHOTOS</p>
@@ -38,10 +76,9 @@ function CreateEvent() {
       </div>
       <div className="line"></div>
       
-      <button className="create-event-button">Create Event</button>
+      <button className="create-event-button" onClick={handleCreate}>Create Event</button>
 
     </div>
-    
   )
 }
 
