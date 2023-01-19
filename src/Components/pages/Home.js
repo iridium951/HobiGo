3c2301e28ee9badb3c18550a4557caf6a1f3974b
Anastasia Shulman <iridium951@gmail.com>
2023-01-19 01:21:10 +0100

    Home.js Event Anzeige

--- src/Components/pages/Home.js
+++ src/Components/pages/Home.js
@@ -6,86 +6,99 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [events, setEvents] = useState([]);
+  const [done, setDone] = useState(false);
   var loginToken = sessionStorage.getItem("AUTH_TOKEN");
-  var loginId = sessionStorage.getItem("AUTH_ID");
+ // var loginId = sessionStorage.getItem("AUTH_ID");
 
-  useEffect(() => {
+  //useEffect(() => {
     const fetchData = async () => {
-        const url = `http://localhost:3000/events/${loginId}`;
-        const requestOptions = {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                'Authorization': 'Bearer ' + loginToken
-            }
-        };
-        const response = await fetch(url, requestOptions);
-        const data = await response.json();
-        setEvents(data);
+      if (done) {
+        return;
+      }
+      const url = `http://localhost:3000/events`;
+      const requestOptions = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          'Authorization': 'Bearer ' + loginToken
+        }
+      };
+
+      fetch(url, requestOptions)
+      .then(response => {
+        if (response.ok) {
+          console.log("ok");
+          response.json().then(data => {
+            console.log("data: " + JSON.stringify(data));
+            setEvents(data);
+            setDone(true);
+          });
+        }
+      })
+
+      //const data = await response.json();
+      //console.log("data: " + JSON.stringify(data));
+      //setEvents(data);
     }
-    fetchData();
-}, [loginToken, loginId]);
 
-
-  
+  fetchData();
 
   const schedulecard = events.map((data, index) =>
-  
-  <Link 
-    to={{pathname: `/event/${data._id}`}}
-    state={{title: data.Title, about: data.About, date: data.Date, loc: data.Location, image: data.image}}
-    key={index}
-    style={{color: "black", textDecoration: "none"}}
-  >
-    <EventCard
-      data={data} 
-      image={data.image}
-  />
-  </Link>
-)
 
-  const reccards = events.map((data, index) =>
-  <Link 
-    to={{pathname: `/event/${data.id}`}}
-    state={{title: data.Title, about: data.About, date: data.Date, loc: data.Location, image: data.Image}}
-    key={index}
-    style={{color: "black", textDecoration: "none"}}
-  >
-    <EventCard
-      data={data} 
-    />
-    
-  </Link>
+    <Link
+      to={{ pathname: `/event/${data._id}` }}
+      state={{ title: data.Title, about: data.About, date: data.Date, loc: data.Location, image: data.image }}
+      key={index}
+      style={{ color: "black", textDecoration: "none" }}
+    >
+      <EventCard
+        data={data}
+        image={data.image}
+      />
+    </Link>
   )
 
+  const reccards = events.map((data, index) =>
+    <Link
+      to={{ pathname: `/event/${data.id}` }}
+      state={{ title: data.Title, about: data.About, date: data.Date, loc: data.Location, image: data.Image }}
+      key={index}
+      style={{ color: "black", textDecoration: "none" }}
+    >
+      <EventCard
+        data={data}
+      />
+
+    </Link>
+  )
+
+  //await fetchData();
 
   return (
-    <>
-    <Link 
-      to={{pathname: `/schedule`}}
-      style= {{color: "black", textDecoration:"none"}}
-    >
-    <div className="interests" style={{ fontFamily: "outfit" }}> 
-          Your Schedule 
-    </div>
-    </Link>
-    {schedulecard}
-    <div className="interests" style={{ fontFamily: "outfit" }}> 
-          Near You 
-    </div>
-    
-    <Grid container spacing={12} >      
-    <Grid item xs={12}>
-        <Grid >
-        {reccards}
+      <>
+      <Link
+        to={{ pathname: `/schedule` }}
+        style={{ color: "black", textDecoration: "none" }}
+      >
+        <div className="interests" style={{ fontFamily: "outfit" }}>
+          Your Schedule
+        </div>
+      </Link>
+      {schedulecard}
+      <div className="interests" style={{ fontFamily: "outfit" }}>
+        Near You
+      </div>
+
+      <Grid container spacing={12} >
+        <Grid item xs={12}>
+          <Grid >
+            {reccards}
+          </Grid>
         </Grid>
       </Grid>
-    </Grid>
-   
-    
-    
+
     </>
 
-    
+
   )
 }
