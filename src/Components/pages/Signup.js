7c423badb51479e49f5e6cd0f1b5730cfb96bf71
mrtkfl <mertkafali00@gmail.com>
2023-01-19 20:33:08 +0100

    login signup responsivness

--- src/Components/pages/Signup.js
+++ src/Components/pages/Signup.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import "./Signup.css"
+import "./Login.css"
 import { useState } from 'react';
 import mail from "./assets/mail.png"
 import lock from "./assets/lock.png"
 import name from "./assets/name.png"
 import { Link } from "react-router-dom"
+import { Box } from '@mui/material';
+
 
 function Signup() {
   const [maill, setMail] = useState('');
@@ -51,7 +53,8 @@ function Signup() {
       <div className="header1" style={{ fontFamily: "outfit" }}>
         Habby!
       </div>
-      <div className="loginheader" style={{ fontFamily: "outfit" }}>
+      <Box bgcolor="white" justifyItems="center" marginLeft="5%" marginRight="5%" borderRadius="15px">
+      <div className='login' style={{ fontFamily: "outfit", fontSize: "34px" }}>
         Sign Up
       </div>
       <div className="loginmsg" style={{ fontFamily: "outfit" }}>
@@ -59,10 +62,10 @@ function Signup() {
       </div>
       <div className="center-input">
         <img src={mail} alt="mail" className="ima1"></img>
-        <input className="input-field" type="text" maxlength="35" size={40} value={maill} onChange={handleMail} />
+        <input className="input-field" type="text" value={maill} onChange={handleMail} />
       </div>
       <div className="center-input">
-        <img src={name} alt="name" className="ima1"></img><p></p>
+        <img src={name} alt="name" className="ima1"></img>
         <input className="input-field" type="text" value={namee} onChange={handleName} />
       </div>
       <div className="center-input">
@@ -71,6 +74,7 @@ function Signup() {
       </div>
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
       </div>
+      </Box>
       <div className="button-container1">
         <Link to={{ pathname: `/login` }}>
           <button className="button login-button1" onClick={() => handleSignup()}>
